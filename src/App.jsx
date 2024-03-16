9b366c4c5a1e781a982ca4d8dd6e346d52e9c539
amimtiaz <amemtiaj19@gmail.com>
2024-03-16 18:00:52 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Blogs from './componets/Blogs/Blogs'
+import Header from './componets/Header/Header'
+
+function App() {
+
+  return (
+    <div>
+      <Header></Header>
+      <Blogs></Blogs>
+    </div>
+
+  )
+}
+
+export default App
